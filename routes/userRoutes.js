e559c3a1b4cb775f42031d5b76d005ef248ccfa0
Jignesh <jignesh.sharma.707@gmail.com>
2023-08-04 00:06:43 +0530

    login and register api -created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,22 +3,51 @@ const User = require('../models/User');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
 router.get('', (req, res) => {
   res.send('User routes are working');
 });
 
 // Register user
 router.post('/register', async (req, res) => {
-  const { name, email, password } = req.body;
   try {
-    const user = new User.create({ name, email, password });
+    const { name, email, password } = req.body;
+
+    const user = new User({ name, email, password });
     await user.save();
-    res.status(201).send({ user, message: 'User created Successfully' });
+    res.status(201).send({ user, message: 'User Created Successfully' });
   } catch (err) {
     res.status(400).send({ error: err });
   }
 });
 
 // Login user
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new Error('Unable to login , invalid credentials');
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new Error('Unable to login , invalid credentials');
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id.toString(),
+      },
+      process.env.JWT_SECRET_KEY
+    );
+
+    res.send({ user, token, message: 'Logged in successfully' });
+  } catch (err) {
+    res.status(400).send({ error: err });
+  }
+});
 
 module.exports = router;
